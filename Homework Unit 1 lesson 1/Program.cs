4b8e27e658e545708d1af665acbf482a42a6ce1b
ArtNord <artem.kam96@mail.ru>
2021-06-07 17:37:31 +1200

    lesson 1

---
+++ Homework Unit 1 lesson 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data1 = DateTime.Now;
+            string name = Environment.UserName;
+            Console.WriteLine("Привет," + (name));
+            Console.WriteLine("Сегодня" + (data1));
+        }
+    }
+}
